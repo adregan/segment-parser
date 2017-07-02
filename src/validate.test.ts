489b6e3e3573894c956ba4314937889c2543438b
Duncan Regan <duncanregan@gmail.com>
2017-07-02 12:16:20 -0400

    Renames to make checks more consistent

--- src/validate.test.ts
+++ src/validate.test.ts
@@ -1,17 +1,18 @@
-import { sizeMatchesPosition } from './validate';
+import { positionsInvalid, createOverlapCheck, sizeInvalid } from './validate';
 
-describe('sizeMatchesPosition', () => {
-  it('returns false if the positions do not cover the size provided', () => {
-    expect(sizeMatchesPosition(1, 4, 5)).toBeFalsy();
-    expect(sizeMatchesPosition(23, 32, 9)).toBeFalsy();
-    expect(sizeMatchesPosition(56, 63, 5)).toBeFalsy();
-    expect(sizeMatchesPosition(120, 215, 95)).toBeFalsy();
-  })
+describe('sizeInvalid', () => {
+  it('returns true if the positions do not cover the size provided', () => {
+    expect(sizeInvalid(1, 4, 5)).toBeTruthy();
+    expect(sizeInvalid(23, 32, 9)).toBeTruthy();
+    expect(sizeInvalid(56, 63, 5)).toBeTruthy();
+    expect(sizeInvalid(120, 215, 95)).toBeTruthy();
+  });
 
-  it('returns true if the positions do cover the size provided', () => {
-    expect(sizeMatchesPosition(1, 4, 4)).toBeTruthy();
-    expect(sizeMatchesPosition(23, 32, 10)).toBeTruthy();
-    expect(sizeMatchesPosition(56, 63, 8)).toBeTruthy();
-    expect(sizeMatchesPosition(120, 215, 96)).toBeTruthy();
-  })
-})
+  it('returns false if the positions do cover the size provided', () => {
+    expect(sizeInvalid(1, 4, 4)).toBeFalsy();
+    expect(sizeInvalid(23, 32, 10)).toBeFalsy();
+    expect(sizeInvalid(56, 63, 8)).toBeFalsy();
+    expect(sizeInvalid(120, 215, 96)).toBeFalsy();
+  });
+});
+
--- src/validate.ts
+++ src/validate.ts
@@ -1,7 +1,4 @@
-export function sizeMatchesPosition(
-  start: number,
-  end: number,
-  size: number
-): boolean {
-  return end - start + 1 === size;
+export function sizeInvalid(start: number, end: number, size: number): boolean {
+  return end - start + 1 !== size;
+}
 }
