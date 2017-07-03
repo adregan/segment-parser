f6fdf06f2b45bc040198a83b9eac5cfa346503a7
Duncan Regan <duncanregan@gmail.com>
2017-07-02 20:56:22 -0400

    Adds a sample header to parse

--- demo.js
+++ demo.js
@@ -2,5 +2,13 @@ const SegmentParser = require('./lib');
 const headerRecord = require('./configs/headerRecord.json');
 const baseSegment = require('./configs/baseSegment.json');
 
-const HeaderParser = new SegmentParser(headerRecord);
-const BaseSegmentParser = new SegmentParser(baseSegment);
+const headerSample = `0560HEADER341234567890               333333333303162017100419840701201707022017BANANAS FOR LYFE                        1234 MAIN ST NOTOWN                                                                             2125555555                                                                                                                                                                                                         `;
+
+function main() {
+  const HeaderParser = new SegmentParser(headerRecord);
+  const BaseSegmentParser = new SegmentParser(baseSegment);
+
+  console.log(HeaderParser.parse(headerSample));
+}
+
+main();
