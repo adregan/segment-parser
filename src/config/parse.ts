804b44b1bb61626a94f9d463b10f26d2f1c5f68d
Duncan Regan <duncanregan@gmail.com>
2017-07-02 22:54:26 -0400

    Adds docs

--- src/config/parse.ts
+++ src/config/parse.ts
@@ -1,6 +1,11 @@
 import camelCase from 'lodash/camelCase';
 import { Config, ParserConfig, Field, ParserField } from './index';
 
+/**
+ * parses a field config into a parser config
+ * @param {Config} config - The configuration object
+ * @return {ParserConfig} config the segment parser uses to parse data
+ */
 export default function parse(config: Config): ParserConfig {
   const { name: rawName, fields: rawFields, size } = config;
   const fields = rawFields.map(parseField);
@@ -9,6 +14,11 @@ export default function parse(config: Config): ParserConfig {
   return { fields, name, size };
 }
 
+/**
+ * parses an individual field into a parser field
+ * @param {Field} field - the raw field config
+ * @return {ParserField} the field info used to parse data
+ */
 export function parseField(field: Field): ParserField {
   const { end, fieldId: id, fieldName, start, fieldType } = field;
   const range: [number, number] = [start - 1, end];
--- src/config/validate.ts
+++ src/config/validate.ts
@@ -2,6 +2,11 @@ import range from 'lodash/range';
 import isEmpty from 'lodash/isEmpty';
 import { Config } from './index';
 
+/**
+ * will validate a config
+ * @param {Config} config - the config object
+ * @return {Config} the config object (if valid, otherwise throws)
+ */
 export default function validate(config: Config): Config {
   const { fields, size, name } = config;
   if (isEmpty(fields) || !size || isEmpty(name)) {
@@ -45,17 +50,41 @@ export default function validate(config: Config): Config {
   return config;
 }
 
+/**
+ * checks if the start is bigger than the end
+ * @param {number} start - the first position
+ * @param {number} end - the final position
+ * @return {bool}
+ */
 export function positionsInvalid(start: number, end: number): boolean {
   return start > end;
 }
 
+/**
+ * checks if the range doesn't match the size
+ * @param {number} start - the first position
+ * @param {number} end - the final position
+ * @param {number} size - the size of the field
+ * @return {bool}
+ */
 export function sizeInvalid(start: number, end: number, size: number): boolean {
   return end - start + 1 !== size;
 }
 
+/**
+ * creates a new checkOverlap function (closes over its own state)
+ * @return {function} checkOverlap function
+ */
 export function createOverlapCheck() {
   let seenRanges = new Map();
-
+  /**
+   * checks if the any fields overlap
+   * @param {number} start - the first position
+   * @param {number} end - the final position
+   * @param {string} name - the name of the field
+   * @return {overlaps: boolean, conflicting?: string} An object with a boolean
+   *     and the name of the field that overlaps to display for error purposes
+   */
   return function checkOverlap(
     start: number,
     end: number,
